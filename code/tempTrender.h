6125b6fd9647850ebe3ebd950a122fa1e68c8659
Marcus <marcus@skolds.com>
2017-11-01 14:08:25 +0100

    Had commit before I could pull from the master branch

--- code/tempTrender.h
+++ code/tempTrender.h
@@ -12,7 +12,7 @@ class tempTrender {
 	
 	//void tempOnDay(int monthToCalculate, int dayToCalculate); //Make a histogram of the temperature on this day
 	//void tempOnDay(int dateToCalculate); //Make a histogram of the temperature on this date
-	//void tempPerDay(); //Make a histogram of the average temperature of each day of the year
+	void tempPerDay(); //Make a histogram of the average temperature of each day of the year
 	//void hotCold(); //Make a histogram of the hottest and coldest day of the year
 	//void tempPerYear(int yearToExtrapolate); //Make a histogram of average temperature per year, then fit and extrapolate to the given year
 
