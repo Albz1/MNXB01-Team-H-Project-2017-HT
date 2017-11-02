27cbdf1439cb5b8d274ac4509af1641e67125526
Akeronian <fte15hh1@student.lu.se>
2017-11-02 20:40:08 +0100

    fixed my clientside.

---
+++ code/rootlogon_C.d
@@ -0,0 +1,40 @@
+
+# DO NOT DELETE
+
+./rootlogon_C.so: /usr/include/root/TF1.h /usr/include/root/TFormula.h
+./rootlogon_C.so: /usr/include/root/TNamed.h /usr/include/root/TObject.h
+./rootlogon_C.so: /usr/include/root/Rtypes.h /usr/include/root/RConfig.h
+./rootlogon_C.so: /usr/include/root/RVersion.h /usr/include/root/DllImport.h
+./rootlogon_C.so: /usr/include/root/Rtypeinfo.h /usr/include/root/snprintf.h
+./rootlogon_C.so: /usr/include/root/strlcpy.h
+./rootlogon_C.so: /usr/include/root/TGenericClassInfo.h
+./rootlogon_C.so: /usr/include/root/TSchemaHelper.h
+./rootlogon_C.so: /usr/include/root/TStorage.h
+./rootlogon_C.so: /usr/include/root/TVersionCheck.h
+./rootlogon_C.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
+./rootlogon_C.so: /usr/include/root/TString.h /usr/include/root/TMathBase.h
+./rootlogon_C.so: /usr/include/root/TBits.h /usr/include/root/TObjArray.h
+./rootlogon_C.so: /usr/include/root/TSeqCollection.h
+./rootlogon_C.so: /usr/include/root/TCollection.h
+./rootlogon_C.so: /usr/include/root/TIterator.h /usr/include/root/TAttLine.h
+./rootlogon_C.so: /usr/include/root/TAttFill.h /usr/include/root/TAttMarker.h
+./rootlogon_C.so: /usr/include/root/Math/ParamFunctor.h
+./rootlogon_C.so: /usr/include/root/TH1.h /usr/include/root/TAxis.h
+./rootlogon_C.so: /usr/include/root/TAttAxis.h /usr/include/root/TArrayD.h
+./rootlogon_C.so: /usr/include/root/TArray.h /usr/include/root/TArrayC.h
+./rootlogon_C.so: /usr/include/root/TArrayS.h /usr/include/root/TArrayI.h
+./rootlogon_C.so: /usr/include/root/TArrayF.h /usr/include/root/Foption.h
+./rootlogon_C.so: /usr/include/root/TVectorFfwd.h
+./rootlogon_C.so: /usr/include/root/TVectorDfwd.h
+./rootlogon_C.so: /usr/include/root/TFitResultPtr.h
+./rootlogon_C.so: /usr/include/root/TStyle.h /usr/include/root/TAttText.h
+./rootlogon_C.so: /usr/include/root/TMath.h /usr/include/root/TError.h
+./rootlogon_C.so: /usr/include/root/TCanvas.h /usr/include/root/TPad.h
+./rootlogon_C.so: /usr/include/root/TVirtualPad.h /usr/include/root/TAttPad.h
+./rootlogon_C.so: /usr/include/root/TVirtualX.h /usr/include/root/GuiTypes.h
+./rootlogon_C.so: /usr/include/root/Buttons.h /usr/include/root/TQObject.h
+./rootlogon_C.so: /usr/include/root/TAttBBox2D.h /usr/include/root/TPoint.h
+./rootlogon_C.so: /usr/include/root/TAttCanvas.h
+./rootlogon_C.so: /usr/include/root/TCanvasImp.h
+./rootlogon_C.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
+rootlogon_C__ROOTBUILDVERSION= 5.34/30
